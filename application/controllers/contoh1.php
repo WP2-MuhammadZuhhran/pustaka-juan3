af0e54072b2f100692987b9fcda572d950ba26ab
Muhammad Zuhran <muhammadzuhran08@gmail.com>
2023-10-02 18:33:12 +0700

    folder juan

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1> HALOOOOO!!!! </h1>";
+        echo "Nama Saya Muhammad Zuhran
+              Saya Tinggal DiDesa Slipi";
+    }
+}
